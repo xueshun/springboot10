2c9aa242477c377588651bdc07ce3c89bd4f366f
xues <xues@91wangjing.com>
2018-02-07 17:20:18 +0800

    spring boot mybatis集成【xml】

--- src/main/java/com/xue/dao/RoncooUserMapper.java
+++ src/main/java/com/xue/dao/RoncooUserMapper.java
@@ -1,21 +1,26 @@
 package com.xue.dao;
 
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.annotations.Param;
 
 import com.xue.bean.RoncooUser;
 
 @Mapper
 public interface RoncooUserMapper {
-	
-	@Insert(value="insert into roncoo_user (name, create_time) values (#{name,jdbcType=VARCHAR}, #{createTime,jdbcType=TIMESTAMP})")
-	int insert(RoncooUser record);
-	
-	@Select(value = "select id, name, create_time from roncoo_user where id = #{id,jdbcType=INTEGER}")
-	@Results(value = { @Result(column = "create_time", property = "createTime", jdbcType = JdbcType.TIMESTAMP) })
-	RoncooUser selectByPrimaryKey(Integer id);
+
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(RoncooUser record);
+
+    int insertSelective(RoncooUser record);
+
+    RoncooUser selectByPrimaryKey(Integer id);
+
+
+    int updateByPrimaryKeySelective(RoncooUser record);
+
+    int updateByPrimaryKey(RoncooUser record);
 }
